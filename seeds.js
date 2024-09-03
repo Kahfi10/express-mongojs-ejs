c0466c54fe71cee5d61e19b8ca8b8674170c301a
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-09-03 16:05:43 +0800

    first commit

---
+++ seeds.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Product = require('./models/products');
+
+mongoose.connect('mongodb://127.0.0.1/shop_db').then((result) => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log(err);
+});
+
+const seedProduct = [
+    {
+        name: 'T-shirt',
+        price: 150000,
+        color: 'Red',
+        size: 'S'
+    },
+    {
+        name: 'Jacket',
+        price: 300000,
+        color: 'Black',
+        size: 'M'
+    },
+    {
+        name: 'Hoodie',
+        price: 250000,
+        color: 'Grey',
+        size: 'L'
+    }
+]
+
+Product.insertMany(seedProduct).then((result) => {
+    console.log(result)
+}).catch((err) => {
+    console.log(err)
+})
