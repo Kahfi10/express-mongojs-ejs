3a8722da96d30d27e2224618a8cbb75718eeb6d1
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-09-11 07:35:28 +0800

    refactor: Add error handling for product routes
    - Refactor the code to add error handling for product routes
    - Add a new function wrapAsync to handle asynchronous operations and catch errors
    - Use the wrapAsync function for the GET, PUT, and DELETE routes of the /products endpoint
    - Update the error handling in the GET and PUT routes to use the wrapAsync function
    - Remove the try-catch blocks in the GET and PUT routes and replace them with the wrapAsync function
    - Update the error messages in the catch blocks to be more descriptive

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const ErrorHandler = require('./ErrorHandler');
 
 const Product = require('./models/products');
+const { wrap } = require('module');
 
 mongoose.connect('mongodb://127.0.0.1/shop_db').then((result) => {
     console.log('Connected to MongoDB');
@@ -18,11 +19,17 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+function wrapAsync(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(err => next(err))
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Hello Kahfi');
 })
 
-app.get('/products', async (req, res) => {
+app.get('/products', wrapAsync(async (req, res) => {
     const { category } = req.query;
     if (category) {
         const products = await Product.find({ category });
@@ -31,7 +38,7 @@ app.get('/products', async (req, res) => {
         const products = await Product.find({});
         res.render('products/index', { products, category:'All' });
     }
-})
+}))
 app.get('/products/create', (req, res) => {
     res.render('products/create');
 })
@@ -42,44 +49,28 @@ app.post('/products', async (req, res) => {
     res.redirect(`/products/${product._id}`);
 })
 
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.render('products/show', { product });
-    } catch (error) {
-        next(new ErrorHandler(404, 'Product Not Found'));
-    }
-})
-app.get('/products/:id/edit', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.render('products/edit', { product });
-    } catch (error) {
-        next(new ErrorHandler(412, 'Gagal Edit Product'));
-    }
-})
+app.get('/products/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+}))
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product });
+}))
 
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
+app.put('/products/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
     res.redirect(`/products/${product._id}`);
-    } catch (error) {
-        next(new ErrorHandler(412, 'Gagal Update Product'));
-    }
-})
+}))
 
-app.delete('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params
-        await Product.findByIdAndDelete(id)
-        res.redirect('/products')
-    } catch (error) {
-        next(new ErrorHandler(412, 'Gagal Delete Product'));
-    }
-})
+app.delete('/products/:id', wrapAsync(async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+}))
 
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong' } = err;
